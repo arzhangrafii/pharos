1ea649e3f2af4abf65f8ce1f69103b4e40bfe3d4
arzhangrafii <arzhang.rafii@mail.utoronto.ca>
2020-06-20 00:06:06 +0000

    reset level fixed

--- ip_repo/hls_ips/scripts/latency_monitor/slave_timer/slave_timer.tcl
+++ ip_repo/hls_ips/scripts/latency_monitor/slave_timer/slave_timer.tcl
@@ -8,6 +8,6 @@ add_files $root_dir/src/$ip_name/$module_name/$module_name.cpp
 open_solution "solution1"
 set_part {xczu19eg-ffvc1760-2-i} -tool vivado
 create_clock -period 10 -name default
-config_rtl -reset all low -reset all
+config_rtl -reset_level low -reset all
 csynth_design
 export_design -rtl verilog
