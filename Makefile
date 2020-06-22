7e8c374e1f8a0978c92c5ed3ef12e12a251ff946
arzhangrafii <arzhang.rafii@mail.utoronto.ca>
2020-06-22 22:03:50 +0000

    makefiles updatet. bandwidth example tcl finished

--- Makefile
+++ Makefile
@@ -2,5 +2,9 @@ Perf_Monitor_IPCore:
 	$(MAKE) -C ip_repo/hls_ips
 	$(MAKE) -C ip_repo/vivado_ips
 
+bandwidth_monitor_example:
+	$(MAKE) -C examples/hls_ips
+	$(MAKE) -C examples bandwidth_monitor
+
 clean_all:
 	$(MAKE) -C hls_ips clean
--- examples/scripts/bandwidth_monitor.tcl
+++ examples/scripts/bandwidth_monitor.tcl
@@ -17,6 +17,9 @@ update_ip_catalog
 #instantiate packet sink and packet source
 create_bd_cell -type ip -vlnv xilinx.com:hls:packet_source:1.0 packet_source_0
 create_bd_cell -type ip -vlnv xilinx.com:hls:packet_sink:1.0 packet_sink_0
+create_bd_cell -type ip -vlnv xilinx.com:hls:data_stream_compressor:1.0 data_stream_compress_0
+create_bd_cell -type ip -vlnv xilinx.com:hls:data_stream_compressor:1.0 data_stream_compress_1
+create_bd_cell -type ip -vlnv xilinx.com:hls:data_stream_compressor:1.0 data_stream_compress_2
 
 #instantiate packet snooper by running the tcl script
 source $root_dir/src/util/connect_packet_snooper_512.tcl
@@ -48,6 +51,8 @@ set_property -dict [list CONFIG.NUM_MI {5} CONFIG.NUM_SI {1}] [get_bd_cells smar
 connect_bd_intf_net [get_bd_intf_pins zynq_ultra_ps_e_0/M_AXI_HPM0_FPD] [get_bd_intf_pins smartconnect_0/S00_AXI]
 connect_bd_net [get_bd_pins smartconnect_0/aclk] [get_bd_pins zynq_ultra_ps_e_0/pl_clk0]
 connect_bd_net [get_bd_pins smartconnect_0/aresetn] [get_bd_pins zynq_ultra_ps_e_0/pl_resetn0]
+
+
 #gpios
 create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:2.0 axi_gpio_0
 create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:2.0 axi_gpio_1
@@ -66,6 +71,52 @@ connect_bd_net [get_bd_pins axi_gpio_0/s_axi_aclk] [get_bd_pins zynq_ultra_ps_e_
 connect_bd_net [get_bd_pins axi_gpio_0/s_axi_aresetn] [get_bd_pins zynq_ultra_ps_e_0/pl_resetn0]
 connect_bd_net [get_bd_pins axi_gpio_1/s_axi_aresetn] [get_bd_pins zynq_ultra_ps_e_0/pl_resetn0]
 
+connect_bd_net [get_bd_pins data_stream_compress_0/aclk] [get_bd_pins zynq_ultra_ps_e_0/pl_clk0]
+connect_bd_net [get_bd_pins data_stream_compress_0/aresetn] [get_bd_pins zynq_ultra_ps_e_0/pl_resetn0]
+connect_bd_net [get_bd_pins data_stream_compress_1/aclk] [get_bd_pins zynq_ultra_ps_e_0/pl_clk0]
+connect_bd_net [get_bd_pins data_stream_compress_1/aresetn] [get_bd_pins zynq_ultra_ps_e_0/pl_resetn0]
+connect_bd_net [get_bd_pins data_stream_compress_2/aclk] [get_bd_pins zynq_ultra_ps_e_0/pl_clk0]
+connect_bd_net [get_bd_pins data_stream_compress_2/aresetn] [get_bd_pins zynq_ultra_ps_e_0/pl_resetn0]
+
 #FIFOs
 create_bd_cell -type ip -vlnv xilinx.com:ip:axi_fifo_mm_s:4.1 axi_fifo_mm_s_0
 set_property -dict [list CONFIG.C_USE_TX_DATA {0} CONFIG.C_USE_TX_CTRL {0}] [get_bd_cells axi_fifo_mm_s_0]
+connect_bd_net [get_bd_pins axi_fifo_mm_s_0/s_axi_aclk] [get_bd_pins zynq_ultra_ps_e_0/pl_clk0]
+connect_bd_net [get_bd_pins axi_fifo_mm_s_0/s_axi_aresetn] [get_bd_pins zynq_ultra_ps_e_0/pl_resetn0]
+
+create_bd_cell -type ip -vlnv xilinx.com:ip:axi_fifo_mm_s:4.1 axi_fifo_mm_s_1
+set_property -dict [list CONFIG.C_USE_TX_DATA {0} CONFIG.C_USE_TX_CTRL {0}] [get_bd_cells axi_fifo_mm_s_1]
+connect_bd_net [get_bd_pins axi_fifo_mm_s_1/s_axi_aclk] [get_bd_pins zynq_ultra_ps_e_0/pl_clk0]
+connect_bd_net [get_bd_pins axi_fifo_mm_s_1/s_axi_aresetn] [get_bd_pins zynq_ultra_ps_e_0/pl_resetn0]
+
+create_bd_cell -type ip -vlnv xilinx.com:ip:axi_fifo_mm_s:4.1 axi_fifo_mm_s_2
+set_property -dict [list CONFIG.C_USE_TX_DATA {0} CONFIG.C_USE_TX_CTRL {0}] [get_bd_cells axi_fifo_mm_s_2]
+connect_bd_net [get_bd_pins axi_fifo_mm_s_2/s_axi_aclk] [get_bd_pins zynq_ultra_ps_e_0/pl_clk0]
+connect_bd_net [get_bd_pins axi_fifo_mm_s_2/s_axi_aresetn] [get_bd_pins zynq_ultra_ps_e_0/pl_resetn0]
+
+#assign outputs
+connect_bd_intf_net -boundary_type upper [get_bd_intf_pins packet_snooper_0/number_of_flits] [get_bd_intf_pins data_stream_compress_0/in_stream_V]
+connect_bd_intf_net -boundary_type upper [get_bd_intf_pins packet_snooper_0/cycle_count] [get_bd_intf_pins data_stream_compress_1/in_stream_V]
+connect_bd_intf_net -boundary_type upper [get_bd_intf_pins packet_snooper_0/packet_size_average] [get_bd_intf_pins data_stream_compress_2/in_stream_V]
+connect_bd_intf_net [get_bd_intf_pins data_stream_compress_0/out_stream_V] [get_bd_intf_pins axi_fifo_mm_s_0/AXI_STR_RXD]
+connect_bd_intf_net [get_bd_intf_pins data_stream_compress_1/out_stream_V] [get_bd_intf_pins axi_fifo_mm_s_1/AXI_STR_RXD]
+connect_bd_intf_net [get_bd_intf_pins data_stream_compress_2/out_stream_V] [get_bd_intf_pins axi_fifo_mm_s_2/AXI_STR_RXD]
+
+connect_bd_intf_net [get_bd_intf_pins smartconnect_0/M02_AXI] [get_bd_intf_pins axi_fifo_mm_s_0/S_AXI]
+connect_bd_intf_net [get_bd_intf_pins smartconnect_0/M03_AXI] [get_bd_intf_pins axi_fifo_mm_s_1/S_AXI]
+connect_bd_intf_net [get_bd_intf_pins smartconnect_0/M04_AXI] [get_bd_intf_pins axi_fifo_mm_s_2/S_AXI]
+
+#reset
+disconnect_bd_net /zynq_ultra_ps_e_0_pl_resetn0 [get_bd_pins zynq_ultra_ps_e_0/pl_resetn0]
+create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 proc_sys_reset_0
+connect_bd_net [get_bd_pins proc_sys_reset_0/interconnect_aresetn] [get_bd_pins packet_source_0/aresetn]
+connect_bd_net [get_bd_pins proc_sys_reset_0/ext_reset_in] [get_bd_pins zynq_ultra_ps_e_0/pl_resetn0]
+connect_bd_net [get_bd_pins proc_sys_reset_0/slowest_sync_clk] [get_bd_pins zynq_ultra_ps_e_0/pl_clk0]
+
+assign_bd_address
+validate_bd_design
+make_wrapper -files [get_files $project_dir/$project_name/${project_name}.srcs/sources_1/bd/${project_name}/${project_name}.bd] -top
+add_files -norecurse $project_dir/$project_name/${project_name}.srcs/sources_1/bd/${project_name}/hdl/${project_name}_wrapper.v
+save_bd_design
+close_project
+exit
